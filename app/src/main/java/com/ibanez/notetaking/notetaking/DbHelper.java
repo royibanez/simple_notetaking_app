e201b3d4494ded3c2281ae31feecf2980c4de954
royibanez <royven27@gmail.com>
2018-10-27 17:44:56 +0800

    My simple note taking app demo

---
+++ app/src/main/java/com/ibanez/notetaking/notetaking/DbHelper.java
@@ -0,0 +1,82 @@
+package com.ibanez.notetaking.notetaking;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import java.util.Calendar;
+
+
+public class DbHelper extends SQLiteOpenHelper {
+    public static final String TAG = "DbHelper";
+    public static final String DB_NAME = "app_notes.db";
+    public static final int DB_VERSION = 1;
+
+    public static final String TABLE_NOTES = "notes";
+    public static final String COL_ID = "id";
+    public static final String COL_TITLE = "title";
+    public static final String COL_BODY = "body";
+    public static final String COL_CREATED_AT = "created_at";
+    public static final String COL_UPDATED_AT = "updated_at";
+
+    public DbHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createNotesTable = "CREATE TABLE "+TABLE_NOTES+"(" +
+                COL_ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+                COL_TITLE + " TEXT, " +
+                COL_BODY + " TEXT, " +
+                COL_CREATED_AT + " TEXT, " +
+                COL_UPDATED_AT + " TEXT" +
+                ")";
+
+        db.execSQL(createNotesTable);
+        Log.d(TAG, createNotesTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    // inserting a note
+    public long insertNote(SQLiteDatabase db, String title, String body){
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH) + 1;
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        ContentValues cv = new ContentValues();
+        cv.put(COL_TITLE, title);
+        cv.put(COL_BODY, body);
+        cv.put(COL_CREATED_AT, year + "-" + month + "-" + day);
+        cv.put(COL_UPDATED_AT, year + "-" + month + "-" + day);
+
+        long id = db.insert(TABLE_NOTES, null, cv);
+        return id;
+    }
+
+    public boolean updateNote(SQLiteDatabase db, int id, String title, String body){
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH) + 1;
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        ContentValues cv = new ContentValues();
+        cv.put(COL_TITLE, title);
+        cv.put(COL_BODY, body);
+        cv.put(COL_UPDATED_AT, year + "-" + month + "-" + day);
+
+        int numberOfRowsAffected = db.update(TABLE_NOTES, cv, COL_ID+"="+id, null);
+        return numberOfRowsAffected > 0;
+    }
+
+    public boolean deleteNote(SQLiteDatabase db, int id){
+        int numberOfRowsDeleted = db.delete(TABLE_NOTES, COL_ID + "=" + id, null);
+        return numberOfRowsDeleted > 0;
+    }
+}
---
+++ app/src/main/java/com/ibanez/notetaking/notetaking/MainActivity.java
@@ -0,0 +1,56 @@
+package com.ibanez.notetaking.notetaking;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private DbHelper dbHelper = null;
+    private SQLiteDatabase db = null;
+    public static final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize the DBHelper
+        dbHelper = new DbHelper(this);
+        // Get the writable database connection
+        db = dbHelper.getWritableDatabase();
+
+        // insert note sample
+        long id = dbHelper.insertNote(db, "My First Note", "This is my very first note!");
+        if(id > 0){
+            Log.d(TAG, "onCreate: Note was inserted");
+        }
+
+        // This is how you close the SQLiteDatabase connection
+        db.close();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.add_note){
+            // Start the add note activity here
+            Intent noteIntent = new Intent(this, NotesActivity.class);
+            startActivity(noteIntent);
+        }
+
+        return true;
+    }
+}
